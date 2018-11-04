1f5941fb3b291aa6af109d0b777dbd4abb19cae6
Mojtaba <mojtabamarashee@gmail.com>
2018-11-04 19:28:29 +0330

    add package

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import SystemSetting from 'react-native-system-setting'
 export default class App extends React.Component {
   render() {
     return (
